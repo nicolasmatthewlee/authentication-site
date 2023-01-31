24af0a5297f8cffb4cacf19e0e2e2d23f663b881
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-01-31 11:00:48 -0800

    setup express application and developer environment with nodemon (scripts:'devstart':'nodemon app.js'); serving client with app.use(express.static)

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+// create application
+const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+// start server
+app.listen(8000, () => console.log("listening at port 8000"));
