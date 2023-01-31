d47c97fff22d451147c87aa222d272a65f37cdf4
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-01-31 11:24:53 -0800

    setup mongoDB database, connection to mongoDB, created model for user with username, password

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+
+const User = require("./models/user");
+
+// connect to MongoDB
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB).catch((err) => {
+  throw err;
+});
+mongoose.connection.on("error", (err) => {
+  if (err) throw err;
+});
 
 // create application
 const app = express();
 
+// define routes
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
