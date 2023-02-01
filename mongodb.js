81f51efb5173daeb2d38b93734647d507afa2697
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-02-01 11:21:31 -0800

    added script to modify and access database because Atlas was failing to load data

---
+++ mongodb.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB).catch((err) => {
+  throw err;
+});
+mongoose.connection.on("error", (err) => {
+  if (err) throw err;
+});
+
+const User = require("./models/user");
+
+const performOperations = async () => {
+  try {
+    await User.deleteMany({ username: "adsf" });
+    console.log("Deletion successful.");
+  } catch (err) {
+    console.log(err);
+  }
+
+  User.find().exec((err, result) => {
+    if (err) console.log(err);
+    console.log(result);
+    mongoose.connection.close();
+  });
+};
+
+performOperations();
