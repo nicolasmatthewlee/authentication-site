c3347896814691a1fb46239833b742084b520a9e
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-02-03 12:32:05 -0800

    added loading component for loading data for /home

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -157,127 +157,145 @@ export const Home = (props) => {
 
   return (
     <div>
-      <div
-        className="container-fluid p-3 pt-0"
-        style={{ marginBottom: "50px" }}
-      >
-        <div className="row py-3 shadow-sm d-flex align-items-center">
-          <div className="col">
-            <h1 className="m-0">Welcome, {username}</h1>
-          </div>
-          <div className="col-auto">
-            <button className="btn btn-dark" onClick={handleLogout}>
-              Logout
-            </button>
-          </div>
-        </div>
-
-        <div className="row mt-3 d-flex justify-content-center gx-3">
+      {username && messages ? (
+        <div>
           <div
-            className="d-flex flex-column align-items-center w-100 overflow-auto"
-            style={{ maxWidth: "500px", marginBottom: "190px" }}
+            className="container-fluid p-3 pt-0"
+            style={{ marginBottom: "50px" }}
           >
-            {Array.isArray(messages)
-              ? messages.map((m) =>
-                  status === "admin" ? (
-                    <div className="w-100 mb-3" key={m._id}>
-                      <div className="card rounded-0 rounded-top">
-                        <div className="card-body">
-                          {m.author ? (
-                            <h6 className="card-title">{m.author}</h6>
-                          ) : null}
-                          <p className="card-text">{m.content}</p>
+            <div className="row py-3 shadow-sm d-flex align-items-center">
+              <div className="col">
+                <h1 className="m-0">Welcome, {username}</h1>
+              </div>
+              <div className="col-auto">
+                <button className="btn btn-dark" onClick={handleLogout}>
+                  Logout
+                </button>
+              </div>
+            </div>
+
+            <div className="row mt-3 d-flex justify-content-center gx-3">
+              <div
+                className="d-flex flex-column align-items-center w-100 overflow-auto"
+                style={{ maxWidth: "500px", marginBottom: "190px" }}
+              >
+                {Array.isArray(messages)
+                  ? messages.map((m) =>
+                      status === "admin" ? (
+                        <div className="w-100 mb-3" key={m._id}>
+                          <div className="card rounded-0 rounded-top">
+                            <div className="card-body">
+                              {m.author ? (
+                                <h6 className="card-title">{m.author}</h6>
+                              ) : null}
+                              <p className="card-text">{m.content}</p>
+                            </div>
+                            <div className="card-footer text-muted">
+                              {timeSince(m.datetime)}
+                            </div>
+                          </div>
+                          <button
+                            onClick={() => handleDeleteMessage(m._id)}
+                            className="btn btn-danger rounded-0 rounded-bottom w-100"
+                          >
+                            Delete
+                          </button>
                         </div>
-                        <div className="card-footer text-muted">
-                          {timeSince(m.datetime)}
+                      ) : (
+                        <div className="w-100 mb-3" key={m._id}>
+                          <div className="card">
+                            <div className="card-body">
+                              {m.author ? (
+                                <h6 className="card-title">{m.author}</h6>
+                              ) : (
+                                <h6 className="card-title">anonymous</h6>
+                              )}
+                              <p className="card-text">{m.content}</p>
+                            </div>
+                            <div className="card-footer text-muted">
+                              {timeSince(m.datetime)}
+                            </div>
+                          </div>
                         </div>
-                      </div>
+                      )
+                    )
+                  : null}
+              </div>
+              <div
+                className="fixed-bottom d-flex justify-content-center"
+                style={{ marginBottom: "70px" }}
+              >
+                <div className="col-12" style={{ maxWidth: "550px" }}>
+                  <div className="card w-100 shadow-lg">
+                    <div className="card-body">
+                      <h6 className="card-text">Create Post</h6>
+                      <textarea
+                        className="form-control my-3"
+                        type="text"
+                        placeholder="Your message here..."
+                        onChange={(e) => setMessage(e.target.value)}
+                      />
                       <button
-                        onClick={() => handleDeleteMessage(m._id)}
-                        className="btn btn-danger rounded-0 rounded-bottom w-100"
+                        onClick={handleMessageSubmit}
+                        className="btn btn-primary"
                       >
-                        Delete
+                        Submit
                       </button>
                     </div>
-                  ) : (
-                    <div className="w-100 mb-3" key={m._id}>
-                      <div className="card">
-                        <div className="card-body">
-                          {m.author ? (
-                            <h6 className="card-title">{m.author}</h6>
-                          ) : (
-                            <h6 className="card-title">anonymous</h6>
-                          )}
-                          <p className="card-text">{m.content}</p>
-                        </div>
-                        <div className="card-footer text-muted">
-                          {timeSince(m.datetime)}
-                        </div>
-                      </div>
-                    </div>
-                  )
-                )
-              : null}
-          </div>
-          <div
-            className="fixed-bottom d-flex justify-content-center"
-            style={{ marginBottom: "70px" }}
-          >
-            <div className="col-12" style={{ maxWidth: "550px" }}>
-              <div className="card w-100 shadow-lg">
-                <div className="card-body">
-                  <h6 className="card-text">Create Post</h6>
-                  <textarea
-                    className="form-control my-3"
-                    type="text"
-                    placeholder="Your message here..."
-                    onChange={(e) => setMessage(e.target.value)}
-                  />
-                  <button
-                    onClick={handleMessageSubmit}
-                    className="btn btn-primary"
-                  >
-                    Submit
-                  </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div
-        className="position-fixed w-100 left-0 bottom-0 bg-dark text-white d-flex align-items-center px-3 justify-content-center"
-        style={{ height: "50px" }}
-      >
-        {showPasswordInput ? (
-          <div className="input-group" style={{ maxWidth: "150px" }}>
-            <input
-              className="form-control"
-              type="password"
-              name="password"
-              id="password"
-              placeholder="••••••••••••"
-              style={{ height: "30px" }}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button
-              className="btn btn-success py-0 px-2"
-              style={{ height: "30px" }}
-              onClick={handleSubmitPassword}
-            >
-              <i className="bi-caret-right-fill"></i>
-            </button>
-          </div>
-        ) : (
-          <button
-            onClick={() => setShowPasswordInput(true)}
-            className="btn btn-dark py-0"
-            style={{ height: "30px", fontWeight: "500" }}
+          <div
+            className="position-fixed w-100 left-0 bottom-0 bg-dark text-white d-flex align-items-center px-3 justify-content-center"
+            style={{ height: "50px" }}
           >
-            Register Account
-          </button>
-        )}
-      </div>
+            {showPasswordInput ? (
+              <div className="input-group" style={{ maxWidth: "150px" }}>
+                <input
+                  className="form-control"
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••••••"
+                  style={{ height: "30px" }}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button
+                  className="btn btn-success py-0 px-2"
+                  style={{ height: "30px" }}
+                  onClick={handleSubmitPassword}
+                >
+                  <i className="bi-caret-right-fill"></i>
+                </button>
+              </div>
+            ) : (
+              <button
+                onClick={() => setShowPasswordInput(true)}
+                className="btn btn-dark py-0"
+                style={{ height: "30px", fontWeight: "500" }}
+              >
+                Register Account
+              </button>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="container-fluid position-absolute h-100 w-100 gap-2 d-flex flex-column align-items-center justify-content-center">
+          <h1 className="position-absolute top-0 shadow-sm p-3 m-0 w-100">
+            <i className="bi-patch-check-fill" style={{ color: "black" }}></i>{" "}
+            <span className="ms-2">Authentication Corp.</span>
+          </h1>
+          <div
+            className="d-flex flex-column align-items-center position-absolute mt-3"
+            style={{ top: "clamp(60px,40%,40%)" }}
+          >
+            <h1>Loading resources...</h1>
+            <div className="spinner-border"></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
