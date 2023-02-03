7a542300b40082b66561aab2a5920b4118e584f3
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-02-03 11:00:34 -0800

    styled page not found page

--- client/src/pages/page-not-found.js
+++ client/src/pages/page-not-found.js
@@ -1,3 +1,19 @@
 export const PageNotFound = () => {
-  return <h1>Page Not Found</h1>;
+  return (
+    <div>
+      <h1 className="shadow-sm p-3 m-0">
+        <i className="bi-patch-check-fill" style={{ color: "black" }}></i>
+        <span className="ms-3">Authentication Corp.</span>
+      </h1>
+
+      <div className="container-fluid p-3 d-flex justify-content-center">
+        <h1
+          className="position-absolute"
+          style={{ top: "clamp(80px,40%,40%)" }}
+        >
+          Page Not Found
+        </h1>
+      </div>
+    </div>
+  );
 };
